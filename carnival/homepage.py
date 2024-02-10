7b2589dde6784a4378f7543054f5463d6495735d
11byte <156634029+11byte@users.noreply.github.com>
2024-02-10 10:02:30 +0530

    Update homepage.py

--- carnival/homepage.py
+++ carnival/homepage.py
@@ -1,9 +1,27 @@
+# import mysql.connector
+# mydb = mysql.connector.connect(
+#     host='localhost',
+#     user='root',
+#     password='mysql022004',
+#     database='carnival'
+# )
+# mycursor = mydb.cursor()
+# mycursor.execute('SELECT * FROM user_login')
+# users = mycursor.fetchall()
+# for user in users:
+#     print(user)
 import tkinter as tr
-import mysql.connector
-username = "omkar"
-mydb = mysql.connector.connect(host='localhost',username='root',password='mysql022004',database='carnival')
-mycursor = mydb.cursor()
-query = "select * from user_login where username= %s"
-mycursor.execute(query, username)
-row = mycursor.fetchone()
-print(row)
+from tkinter import messagebox as mb
+window = tr.Tk()
+window.title('USER HOME')
+window.geometry('1700x1000')
+window.configure(bg='#BEF4FF')
+
+
+frame = tr.Frame(bg='#126A83')
+event_catl = tr.Label(frame,text='EVENT CATEGORIES',font=['Roman',25],bg='#126A83',fg='white')
+# eventbox = tr.Entry(frame,font=['Arial',13])
+# eventbox.gridrow=
+event_catl.grid(row=0,column=0,columnspan=2, sticky='news',pady=70,padx=70)
+frame.pack()
+window.mainloop()
