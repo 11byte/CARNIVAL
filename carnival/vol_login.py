a2b09913ccfee104d290e6aab759831e55bc7d2f
Omkar Chandgaonkar <156634029+11byte@users.noreply.github.com>
2024-04-22 08:47:18 +0530

    Update vol_login.py

--- carnival/vol_login.py
+++ carnival/vol_login.py
@@ -7,6 +7,17 @@ window.title("ENROLL")
 window.geometry('340x400')
 window.configure(fg_color='#d1d5fd',borderwidth=5)
 
+def center_window(window, width, height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width // 2) - (width // 2)
+    y = (screen_height // 2) - (height // 2)
+    window.geometry(f'{width}x{height}+{x}+{y}')
+
+desired_width = 340
+desired_height = 400
+center_window(window, desired_width, desired_height)
+
 def login():
     username = username_entry.get()
     password = pass_entry.get()
